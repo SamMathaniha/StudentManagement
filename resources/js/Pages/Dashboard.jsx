039c5f5942e854fbbc1bc5060bf1f163bab574f4
SamMathaniha <mathanihasam@gmail.com>
2024-04-01 02:23:55 +0530

    Updated in Image

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -16,8 +16,7 @@ export default function Dashboard({ auth }) {
 
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">You're logged in!</div><br>
-            
+                        <div className="p-6 text-gray-900">You're logged in!</div>
                  
                     </div>    
                 </div>
@@ -30,7 +29,7 @@ export default function Dashboard({ auth }) {
                  {/* Button to add student details */}
                 <div className="py-3 flex justify-center">
                    <a href="/dashboard/students" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                      Add Student Details
+                      View Student Details
                   </a>
               </div>
             </div>
